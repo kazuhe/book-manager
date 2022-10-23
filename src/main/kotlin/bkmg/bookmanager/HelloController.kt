6cba924d7c9a0274432cbe3cc38b432096a1d9ce
kazuhe <omc.ohara@gmail.com>
2022-10-23 17:46:21 +0900

    Hello World

---
+++ src/main/kotlin/bkmg/bookmanager/HelloController.kt
@@ -0,0 +1,14 @@
+package bkmg.bookmanager
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HelloController {
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute("message", "Hello World!")
+        return "index"
+    }
+}
