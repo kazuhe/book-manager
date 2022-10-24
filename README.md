f5ac9ea44649ad0fdac7c228af541dd0c89c2711
kazuhe <omc.ohara@gmail.com>
2022-10-25 00:00:49 +0900

    Add MyBatis

---
+++ README.md
@@ -0,0 +1,7 @@
+# Book manager
+
+書籍管理システム
+
+- ビルドツール: [Gradle](https://docs.gradle.org)
+- フレームワーク: [Spring Boot](https://spring.io/projects/spring-boot)
+- O/R マッパー: [MyBatis](https://mybatis.org/mybatis-3/ja/)
--- src/main/kotlin/bkmg/bookmanager/BookManagerApplication.kt
+++ src/main/kotlin/bkmg/bookmanager/BookManagerApplication.kt
@@ -1,11 +1,34 @@
 package bkmg.bookmanager
 
+import database.UserMapper
+import database.selectByPrimaryKey
+import org.apache.ibatis.io.Resources
+import org.apache.ibatis.session.SqlSessionFactory
+import org.apache.ibatis.session.SqlSessionFactoryBuilder
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class BookManagerApplication
 
-fun main(args: Array<String>) {
-	runApplication<BookManagerApplication>(*args)
+//fun main(args: Array<String>) {
+//	runApplication<BookManagerApplication>(*args)
+//}
+
+fun main() {
+	getUserByPrimaryKey()
+}
+
+fun createSessionFactory(): SqlSessionFactory {
+	val resource = "mybatis-config.xml"
+	val inputStream = Resources.getResourceAsStream(resource)
+	return SqlSessionFactoryBuilder().build(inputStream)
+}
+
+fun getUserByPrimaryKey() {
+	createSessionFactory().openSession().use { session ->
+		val mapper = session.getMapper(UserMapper::class.java)
+		val user = mapper.selectByPrimaryKey(100)
+		println(user)
+	}
 }
